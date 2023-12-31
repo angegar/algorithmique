9d16d811198fcbc152bcfc2c10b7feb0496156fe
Laurent Gil <lg7983@gmail.com>
2023-12-31 16:44:32 +0000

    feat: add reverse insert sort
    Signed-off-by: Laurent Gil <lg7983@gmail.com>

--- tri/insertion/tri-insertion.py
+++ tri/insertion/tri-insertion.py
@@ -20,3 +20,19 @@ def tri_insertion(input_array):
     return input_array
 
 print(tri_insertion(input))
+
+def tri_insertion_reverse(input_array):
+    for j in range(1,len(input_array)):
+        key = input_array[j]
+        # Insert input_array[j] into the sequence input_array[1..j-1]
+        i = j-1
+        while i >= 0  and input_array[i] < key:
+            input_array[i+1] = input_array[i]
+            i = i-1
+        input_array[i+1] = key
+
+        assert input_array[i+1] <= input_array[i] if i > 0 else True
+
+    return input_array
+
+print(tri_insertion_reverse(input))
